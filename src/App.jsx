9942c1b52034175202ac80e9fafc113496ad6d14
Sasha Tatar <sasa.tatar@gmail.com>
2020-12-25 00:27:55 +0100

    Add react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Table } from 'react-bootstrap';
 
 function App() {
     // Create the count state.
@@ -31,6 +32,37 @@ function App() {
                         Learn React
                     </a>
                 </p>
+                <div>
+                    <Table striped bordered hover size="sm">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Username</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td colSpan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
             </header>
         </div>
     );
