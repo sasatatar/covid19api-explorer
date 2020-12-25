f5a0fcf7cfd9539672fdffa15323772e14dab8cb
Sasha Tatar <sasa.tatar@gmail.com>
2020-12-25 08:03:34 +0100

    Implement basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Col, Nav, Table } from 'react-bootstrap';
+import {
+    Redirect,
+    Route,
+    Switch,
+    useHistory,
+    useLocation,
+} from 'react-router-dom';
 import './App.css';
-import { Table } from 'react-bootstrap';
+
+const Chart = () => <h2>Chart</h2>;
+
+const Data = () => (
+    <>
+        <h2>Data</h2>
+        <Table striped bordered hover size="sm">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td colSpan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </Table>
+    </>
+);
 
 function App() {
-    // Create the count state.
-    const [count, setCount] = useState(0);
-    // Create the counter (+1 every second).
-    useEffect(() => {
-        const timer = setTimeout(() => setCount(count + 1), 1000);
-        return () => clearTimeout(timer);
-    }, [count, setCount]);
-    // Return the App component.
+    let { pathname } = useLocation();
+    console.log(pathname);
+
+    let history = useHistory();
+    function handleChange(val) {
+        history.push(val);
+    }
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.jsx</code> and save to reload.
-                </p>
-                <p>
-                    Page has been open for <code>{count}</code> seconds.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
+        <div>
+            <Col md={{ span: 6, offset: 3 }}>
+                <header>
+                    <Nav
+                        variant="pills"
+                        defaultActiveKey={
+                            pathname == '/chart' ? pathname : '/data'
+                        }
+                        onSelect={handleChange}
                     >
-                        Learn React
-                    </a>
-                </p>
-                <div>
-                    <Table striped bordered hover size="sm">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>First Name</th>
-                                <th>Last Name</th>
-                                <th>Username</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Mark</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td colSpan="2">Larry the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </header>
+                        <Nav.Item>
+                            <Nav.Link eventKey="/data">Data</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="/chart">Chart</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </header>
+                <Switch>
+                    <Route path="/chart">
+                        <Chart />
+                    </Route>
+                    <Route path="/data">
+                        <Data />
+                    </Route>
+                    <Route path="*">
+                        <Redirect to="/data" />
+                    </Route>
+                </Switch>
+            </Col>
         </div>
     );
 }
